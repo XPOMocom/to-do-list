e70345fcb8687ded200f15a8d577c391d6305a21
XPOMocom <6355690@gmail.com>
2017-05-03 15:13:16 +0300

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,83 @@
+
+
+// иконка удаление Х
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// удаление не нужных Х
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// отмечаем галочками выполненные
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// добавление задач в список click on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myText").value;
+  var t = document.createTextNode(inputValue);
+
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } 
+  else {
+    document.getElementById("list").appendChild(li);
+  }
+  // --X--
+  document.getElementById("myText").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
